b6b00cf451f34c600860c16204cf894061266bc9
s200036 <gimpo.s200036@ggh.goe.go.kr>
2024-11-03 15:25:57 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -6,7 +6,7 @@ model = joblib.load('linear_regression_model.pkl')
 
 # 2.모델 설명
 st.title('점수 예측 에이전트')
-st.subheader('수강과목수, 공부시간, 공지확인 습관에 따른 점수 예측')
+st.subheader('모델 설명')
 st.write(' - 기계학습 알고리즘 : 로지스틱 회귀 ')
 st.write(' - 학습 데이터 출처 : https://www.kaggle.com/')
 st.write(' - 총 데이터 건 수: **건')
@@ -17,16 +17,16 @@ st.write(' - 인공지능 모델 정확도 : ****')
 # 3.데이터 시각화
 col1, col2, col3 = st.columns(3)  # 몇 개의 컬럼으로 나눌까?
 with col1:
-      st.subheader('수강과목수&점수')
+      st.subheader('데이터시각화1')
       st.image('시각화1.png' )   # 이미지 불러오기
 with col2:
-      st.subheader('공부시간&점수')
+      st.subheader('데이터시각화2')
       st.image('시각화2.png' )   # 이미지 불러오기
 with col3:
-      st.subheader('   상관관계')
+      st.subheader('데이터시각화3')
       st.image('시각화3.png')   # 이미지 불러오기
 
-st.subheader('3. 예측하기')
+st.subheader('모델 활용')
 st.write('**** 다음을 입력하세요.. 인공지능이 당신의 점수를 알려드립니다!')
 
 # 4.사용자 입력
